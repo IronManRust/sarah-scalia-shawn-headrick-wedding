737e814dedd4351accb1412a587c3650a2b2888a
IronManRust <shawn_headrick@yahoo.com>
2017-02-21 13:16:19 -0600

    Crawl Audio

---
+++ page-story/crawl.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    window.setTimeout(function() {
+        var audio = new Audio("crawl.mp3");
+        audio.play();
+    }, 7000);
+
+});
